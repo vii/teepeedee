1e34b6c4a9506cc1c8cb8fc4c041a138b7016a01
John Fremlin <john@fremlin.org>
2003-01-17 17:38:45 +0000

    *** empty log message ***

---
+++ lib/dircontainer.cc
@@ -0,0 +1,43 @@
+#include "dircontainer.hh"
+
+#include "config.h"
+
+#ifndef HAVE_READDIR_R
+static
+int readdir_r(DIR *d, struct dirent *e,
+		struct dirent **r)
+{
+  *r = readdir(d);
+  if(!*r){
+    if(errno)
+      return -1;
+    return 0;
+  }
+  return 0;
+}
+#endif
+
+void
+DirContainer::read_next()
+{
+  if(!_stream)
+    return;
+
+  struct dirent*e;
+  if(readdir_r(_stream,&_entry,&e)){
+    warn("readdir_r");
+    close();
+    return;
+  }
+  if(!e){
+    close();
+    return;
+  }
+  if(e!=&_entry)
+    memcpy(&_entry,e,sizeof _entry);
+      
+  if(_entry.d_name[0] == '.' && (!_entry.d_name[1] || (_entry.d_name[1] == '.' && !_entry.d_name[2])))
+    read_next();
+  
+  _entry.d_name[sizeof(_entry.d_name)-1]=0;
+}
